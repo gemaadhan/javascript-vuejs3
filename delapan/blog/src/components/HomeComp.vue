fa4fcafd2f45e0be45566de0ab8a11a0f9fbeb89
Gema Disperakim <gema.disperakim@gmail.com>
2022-08-31 16:32:12 +0700

    depalan progress

---
+++ delapan/blog/src/components/HomeComp.vue
@@ -0,0 +1,25 @@
+<template>
+  <h1>HomeComp Component {{ "Hello".length }}</h1>
+  <h2>Email : {{ (email = "bukangema@gmail.com") }}</h2>
+  <h2>Mobile : {{ mobile }}</h2>
+  <h2>Name : {{ getName("Gema Nadia Ramadhana") }}</h2>
+</template>
+<script>
+export default {
+  name: "HomeComp",
+  data() {
+    return {
+      email: "gema@gmail.com",
+      mobile: 9999,
+      getName(name) {
+        return name;
+      },
+    };
+  },
+};
+</script>
+<style scoped>
+h1 {
+  color: orange;
+}
+</style>
