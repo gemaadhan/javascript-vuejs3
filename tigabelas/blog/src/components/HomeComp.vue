b374e437a8b886b4fbfa2124a8a9310f1a185af7
Gema Disperakim <gema.disperakim@gmail.com>
2022-09-02 14:27:51 +0700

    tigalbelas ok

---
+++ tigabelas/blog/src/components/HomeComp.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>HomeComp Component</h1>
+  <h1 v-if="show">Condition in VUe JS</h1>
+  <h1 v-else>Condition in VUe JS when false</h1>
+  <button v-on:click="show = !show">Pencet</button>
+</template>
+<script>
+export default {
+  name: "HomeComp",
+  data() {
+    return {
+      show: true,
+    };
+  },
+};
+</script>
+<style scoped>
+h1 {
+  color: orange;
+}
+</style>
