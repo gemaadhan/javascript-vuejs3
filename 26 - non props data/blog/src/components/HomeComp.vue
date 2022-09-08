1c8d276617e8bd4a98bbffb3217a0c74a155166d
Gema Disperakim <gema.disperakim@gmail.com>
2022-09-08 18:14:38 +0700

    26 ok

---
+++ 26 - non props data/blog/src/components/HomeComp.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>
+    Non Props data muncul ketika kita tidak mendefinisikan object props data
+  </h1>
+  <UserComp item="some data" name="user-gema" />
+</template>
+<script>
+import UserComp from "./UserComp.vue";
+export default {
+  name: "HomeComp",
+  components: {
+    UserComp,
+  },
+};
+</script>
+<style scoped>
+h1 {
+  color: orange;
+}
+</style>
---
+++ 26 - non props data/blog/src/components/UserComp.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h3 v-bind="$attrs">Gema</h3>
+    <h3>Nadia</h3>
+    <h3>Ramadhana</h3>
+  </div>
+  <!-- Non props data can view if we add parent tag <div>  -->
+  <!-- if not, we can see the props data when we add  v-bind="attrs"  -->
+</template>
+<script>
+export default {
+  name: "UserComp",
+  //   props: {
+  //     item: String,
+  //   },
+  inheritAttrs: false,
+};
+</script>
+
