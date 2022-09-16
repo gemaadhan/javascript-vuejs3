7f85917479384993ddd93e844a3ad11af40220a1
Gema Disperakim <gema.disperakim@gmail.com>
2022-09-16 10:42:23 +0700

    today

--- resto-project/src/components/Home.vue
+++ resto-project/src/components/Home.vue
@@ -1,18 +1,20 @@
 <template>
-    <h1>
-        Hello User, Welcome on home page
-    </h1>
+  <Header></Header>
+  <h1>Hello User, Welcome on home page</h1>
 </template>
 
 <script>
+import Header from "./Header.vue";
 export default {
-    name: "HomeComp",
-    mounted() {
-
-        let user = localStorage.getItem("user-info");
-        if (!user) {
-            this.$router.push({ name: "SignUp" })
-        }
+  name: "HomeComp",
+  components: {
+    Header,
+  },
+  mounted() {
+    let user = localStorage.getItem("user-info");
+    if (!user) {
+      this.$router.push({ name: "SignUp" });
     }
-}
+  },
+};
 </script>
